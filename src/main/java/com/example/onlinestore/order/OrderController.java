8eaba1303ecdf8733328a11a6f2a6b50f986f785
Mateusz Konopka <mateusz.konopka11@gmail.com>
2023-01-20 15:14:04 +0100

    delete product from order implemented

--- src/main/java/com/example/onlinestore/order/OrderController.java
+++ src/main/java/com/example/onlinestore/order/OrderController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,4 +34,8 @@ public class OrderController {
         return new ResponseEntity<>(orderService.addProductToOrder(requestBody), HttpStatus.CREATED);
     }
 
+    @DeleteMapping("/{orderId}/delete-product/{productId}")
+    public ResponseEntity<OrderDTO> deleteProductFromOrder(@PathVariable Long orderId, @PathVariable Long productId) {
+        return new ResponseEntity<>(orderService.removeProductFromOrder(orderId, productId), HttpStatus.OK);
+    }
 }
--- src/main/java/com/example/onlinestore/order/OrderService.java
+++ src/main/java/com/example/onlinestore/order/OrderService.java
@@ -3,13 +3,13 @@ package com.example.onlinestore.order;
 import java.math.BigDecimal;
 import java.text.MessageFormat;
 import java.util.List;
+import java.util.Optional;
 
 import com.example.onlinestore.exception.NotFoundException;
 import com.example.onlinestore.exception.ProductInTheOrderException;
 import com.example.onlinestore.exception.ProductNotAvailableException;
 import com.example.onlinestore.orderItem.OrderItem;
 import com.example.onlinestore.orderItem.OrderItemRepository;
-import com.example.onlinestore.product.Product;
 import com.example.onlinestore.product.ProductRepository;
 
 import jakarta.transaction.Transactional;
@@ -34,8 +34,7 @@ public class OrderService {
 
     public List<OrderDTO> getAllOrders() {
         return orderRepository.findAll().stream().map(order -> {
-            BigDecimal totalPrice = calculateTotalPrice(order.getOrderItems());
-            return orderMapper.orderToDto(order).withTotalPrice(totalPrice);
+            return orderMapper.orderToDto(order).withTotalPrice(updateTotalPrice(order));
         }).toList();
     }
 
@@ -52,10 +51,11 @@ public class OrderService {
 
         if (!checkIfProductIsAvailable(requestBody)) {
             throw new ProductNotAvailableException(
-                    MessageFormat.format("Product with ID: {0} is not available.", requestBody.getProductId()));
+                    MessageFormat.format("Product with ID: {0} is not available.",
+                            requestBody.getProductId()));
         }
 
-        if (checkIfProductIsAlreadyInTheOrder(findOrder, findProduct)) {
+        if (checkIfProductIsAlreadyInTheOrder(requestBody)) {
             throw new ProductInTheOrderException(
                     MessageFormat.format("Product ID: {0} ({1}) is in the order already.",
                             requestBody.getProductId(), findProduct.getName()));
@@ -67,27 +67,67 @@ public class OrderService {
                             requestBody.getProductId(), findProduct.getStockQty()));
         }
 
-        orderItemRepository.save(OrderItem.builder()
+        var newOrderItem = orderItemRepository.save(OrderItem.builder()
                 .order(findOrder)
                 .product(findProduct)
                 .qty(requestBody.getQty())
                 .build());
 
+        var orderItemsToReturn = Optional.ofNullable(findOrder.getOrderItems()).map(e -> {
+            e.add(newOrderItem);
+            return e;
+        }).orElse(List.of(newOrderItem));
+
         findOrder.setOrderStatus(OrderStatus.PROCESSING);
+        findOrder.setOrderItems(orderItemsToReturn);
         orderRepository.save(findOrder);
 
         findProduct.setStockQty((findProduct.getStockQty() - requestBody.getQty()));
         findProduct.setInStock(findProduct.getStockQty() <= 0 ? false : true);
         productRepository.save(findProduct);
 
-        BigDecimal totalPrice = calculateTotalPrice(findOrder.getOrderItems());
-        return orderMapper.orderToDto(findOrder).withTotalPrice(totalPrice);
+        return orderMapper.orderToDto(findOrder).withTotalPrice(updateTotalPrice(findOrder));
+    }
+
+    @Transactional
+    public OrderDTO removeProductFromOrder(Long orderId, Long productId) {
+
+        var findOrder = orderRepository.findById(orderId)
+                .orElseThrow(() -> new NotFoundException(
+                        MessageFormat.format("Order with ID: {0} not found.", orderId)));
+
+        var findProduct = productRepository.findById(productId)
+                .orElseThrow(() -> new NotFoundException(
+                        MessageFormat.format("Product with ID: {0} not found.", productId)));
+
+        var findOrderItem = orderRepository.findById(orderId).get().getOrderItems().stream()
+                .filter(product -> product.getProduct().getId().equals(productId)).findFirst()
+                .orElseThrow(() -> new NotFoundException(
+                        MessageFormat.format("Product with ID: {0} is not associated with this order.", productId)));
+
+        var orderItemsToReturn = Optional.ofNullable(findOrder.getOrderItems()).map(e -> {
+            e.remove(findOrderItem);
+            return e;
+        }).orElse(List.of(findOrderItem));
+
+        findOrder.setOrderStatus(OrderStatus.PROCESSING);
+        findOrder.setOrderItems(orderItemsToReturn);
+        orderRepository.save(findOrder);
+
+        orderItemRepository.delete(findOrderItem);
+
+        findProduct.setStockQty((findProduct.getStockQty() + findOrderItem.getQty()));
+        findProduct.setInStock(findProduct.getStockQty() <= 0 ? false : true);
+        productRepository.save(findProduct);
+
+        return orderMapper.orderToDto(findOrder).withTotalPrice(updateTotalPrice(findOrder));
     }
 
     // change product qty in the order METHOD
 
-    private Boolean checkIfProductIsAlreadyInTheOrder(Order findOrder, Product findProduct) {
-        return findOrder.getOrderItems().stream().filter(product -> product.getProduct().equals(findProduct))
+    private Boolean checkIfProductIsAlreadyInTheOrder(OrderAddProductDTO requestBody) {
+        return orderRepository.findById(requestBody.getOrderId()).get().getOrderItems().stream()
+                .filter(product -> product.getId().equals(requestBody.getProductId()))
                 .findFirst().isPresent();
     }
 
@@ -108,6 +148,10 @@ public class OrderService {
                 .map(product -> product.getProduct().getPrice()
                         .multiply(BigDecimal.valueOf(product.getQty())))
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    private BigDecimal updateTotalPrice(Order findOrder) {
+        return calculateTotalPrice(findOrder.getOrderItems());
     }
 
 }
