87dcd12d53fbc453ad2fc7581c141d83e0eb56b9
Mateusz Konopka <mateusz.konopka11@gmail.com>
2023-01-25 15:05:54 +0100

    testing in prog

--- src/main/java/com/example/onlinestore/product/ProductService.java
+++ src/main/java/com/example/onlinestore/product/ProductService.java
@@ -25,7 +25,8 @@ public class ProductService {
 
     public ProductDTO getSingleProductById(Long productId) {
         return productRepository.findById(productId).map(productMapper::productToDto)
-                .orElseThrow(() -> new NotFoundException(MessageFormat.format("Product with ID: {0} not found.", productId)));
+                .orElseThrow(() -> new NotFoundException(
+                        MessageFormat.format("Product with ID: {0} not found.", productId)));
     }
 
     public ProductDTO addProduct(ProductCreateDTO productCreateDTO) {
---
+++ src/test/java/com/example/onlinestore/product/ProductServiceTest.java
@@ -0,0 +1,86 @@
+package com.example.onlinestore.product;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import static org.mockito.BDDMockito.given;
+
+import java.math.BigDecimal;
+
+@ExtendWith(MockitoExtension.class)
+public class ProductServiceTest {
+
+    @Mock
+    private ProductRepository productRepository;
+
+    private ProductService productService;
+
+    @Mock
+    ProductMapper productMapper;
+
+    @BeforeEach
+    void setUp() {
+        productService = new ProductService(productRepository, productMapper);
+    }
+
+    @Test
+    void canGetAllProducts() {
+        // when
+        productService.getAllProducts();
+        // then
+        verify(productRepository).findAll();
+    }
+
+    @Test
+    void canCreateProduct() {
+        // given
+        ProductCreateDTO productDto = ProductCreateDTO.builder()
+                .name("ProductName")
+                .price(new BigDecimal("10"))
+                .stockQty(100)
+                .inStock(true)
+                .build();
+
+        // when
+        productService.addProduct(productDto);
+
+        // then
+        ArgumentCaptor<Product> productArgumentCaptor = ArgumentCaptor.forClass(Product.class);
+        verify(productRepository).save(productArgumentCaptor.capture());
+        Product capturedProduct = productArgumentCaptor.getValue();
+        assertThat(capturedProduct.getName()).isEqualTo(productDto.getName());
+    }
+
+    @Test
+    void canDeleteProduct() {
+        // given
+        ProductDTO productDto = ProductDTO.builder()
+                .name("ProductName")
+                .price(new BigDecimal("10"))
+                .stockQty(100)
+                .inStock(true)
+                .build();
+
+        // when
+
+        var newProd = productRepository.save(productMapper.productToDto(productDto));
+
+        given(productRepository.existsById(newProd.getId()))
+                .willReturn(true);
+
+        // when
+        productService.deleteProduct(newProd.getId());
+
+        // then
+        verify(productRepository).deleteById(newProd.getId());
+    }
+
+}
